b3cdd209708077b525fbe5603ff154fb77539449
君忆 <2589263572@qq.com>
2018-07-31 16:44:37 +0800

    modify

--- idc-admin/idc-admin-web/src/main/java/com/iwellmass/idc/model/JobAlarm.java
+++ idc-admin/idc-admin-web/src/main/java/com/iwellmass/idc/model/JobAlarm.java
@@ -1,13 +1,12 @@
 package com.iwellmass.idc.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 
 /**
  * 任务预警
  */
-@Entity
-@Table(name = "t_idc_job_alarm")
+
 public class JobAlarm {
+
+
 
 }
