d409f69d7d7176c9d09306267b96893092731c76
liqingdong <qingdong.li@iwellmass.com>
2018-09-06 17:44:03 +0800

    fegin client annotation

--- idc-server-spring-cloud-starter/src/main/java/com/iwellmass/idc/service/RestIDCJobExecutor.java
+++ idc-server-spring-cloud-starter/src/main/java/com/iwellmass/idc/service/RestIDCJobExecutor.java
@@ -1,5 +1,6 @@
 package com.iwellmass.idc.service;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -8,7 +9,7 @@ import com.iwellmass.idc.model.JobInstance;
 
 public interface RestIDCJobExecutor extends IDCJobExecutorService {
 	
-	@PostMapping
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void execute(@RequestBody JobInstance jobInstance);
 
 }
