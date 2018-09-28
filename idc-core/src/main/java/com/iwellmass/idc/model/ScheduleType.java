931e8a38217212f200075bc2b83816a2e29fbd58
liqingdong <qingdong.li@iwellmass.com>
2018-09-28 11:20:32 +0800

    修复格式化错误

--- idc-core/src/main/java/com/iwellmass/idc/model/ScheduleType.java
+++ idc-core/src/main/java/com/iwellmass/idc/model/ScheduleType.java
@@ -1,6 +1,8 @@
 package com.iwellmass.idc.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import io.swagger.annotations.ApiModelProperty;
@@ -11,7 +13,7 @@ import io.swagger.annotations.ApiModelProperty;
 public enum ScheduleType {
 
 	@ApiModelProperty("月调度")
-	MONTHLY("yyyyMM"),
+	MONTHLY("yyyyMMdd"),
 
 	@ApiModelProperty("周调度")
 	WEEKLY("yyyyMMddHHmmss"),
@@ -24,22 +26,41 @@ public enum ScheduleType {
 
 	@ApiModelProperty("自定义")
 	CUSTOMER("yyyyMMddHHmmss");
-	
+
 	private final DateTimeFormatter fmt;
-	
+
 	private ScheduleType(String pattern) {
 		this.fmt = DateTimeFormatter.ofPattern(pattern);
 	}
-	
+
 	public String format(LocalDateTime loadDate) {
 		if (loadDate == null) {
 			return null;
 		}
 		return loadDate.format(fmt);
 	}
-	
+
 	public LocalDateTime parse(String loadDateStr) {
+		if (loadDateStr == null) {
+			return null;
+		}
+
+		LocalTime min = LocalTime.of(0, 0, 0);
+
+		if (this == ScheduleType.MONTHLY) {
+			if (loadDateStr.length() == 6) {
+				loadDateStr += "01";
+			}
+			return LocalDateTime.of(LocalDate.parse(loadDateStr, fmt).plusMonths(1).minusDays(1), min);
+
+		} else if (this == DAILY) {
+			return LocalDateTime.of(LocalDate.parse(loadDateStr, fmt), min);
+		}
 		return LocalDateTime.parse(loadDateStr, fmt);
 	}
-	
+
+	public static void main(String[] args) {
+		System.out.println(DAILY.parse("20180101"));
+		System.out.println(MONTHLY.parse("201802"));
+	}
 }
