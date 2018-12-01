9f323f482a7d033f294e0c3db38cfd9f646941b0
liqingdong <qingdong.li@iwellmass.com>
2018-12-01 10:25:39 +0800

    task/query 接口

--- idc-app/src/main/java/com/iwellmass/idc/app/controller/TaskController.java
+++ idc-app/src/main/java/com/iwellmass/idc/app/controller/TaskController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iwellmass.common.ServiceResult;
@@ -29,19 +28,19 @@ public class TaskController {
 	@Inject
     private QTaskService taskService;
 	
-	@ApiOperation("获取所有任务")
-	@GetMapping
-	public ServiceResult<PageData<Task>> get(@RequestBody(required = false) TaskQueryVO taskQuery, Pager pager) {
-		PageData<Task> ret = taskService.queryTask(taskQuery, pager);
-		return ServiceResult.success(ret);
-	}
-	
     @ApiOperation("保存任务")
     @PostMapping
     public ServiceResult<TaskKey> add(@RequestBody Task task){
     	taskService.saveTask(task);
         return ServiceResult.success(task.getTaskKey());
     }
+	
+	@ApiOperation("获取所有任务")
+	@PostMapping("/query")
+	public ServiceResult<PageData<Task>> get(@RequestBody(required = false) TaskQueryVO taskQuery, Pager pager) {
+		PageData<Task> ret = taskService.queryTask(taskQuery, pager);
+		return ServiceResult.success(ret);
+	}
     
     @ApiOperation("获取所有工作流子任务")
     @GetMapping("/all-sub-task")
