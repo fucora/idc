a6be2d1075fe0a64f7459d538345336ad4745485
nobita <1057735161@qq.com>
2018-12-14 10:15:50 +0800

    更新init方法.

--- idc-app/src/main/java/com/iwellmass/idc/app/service/InitService.java
+++ idc-app/src/main/java/com/iwellmass/idc/app/service/InitService.java
@@ -1,6 +1,7 @@
 package com.iwellmass.idc.app.service;
 
 import com.iwellmass.common.ServiceResult;
+import com.iwellmass.common.exception.AppException;
 import com.iwellmass.idc.app.controller.InitController;
 import com.iwellmass.idc.app.repo.TaskRepository;
 import com.iwellmass.idc.model.Task;
@@ -18,26 +19,17 @@ import java.util.stream.Collectors;
 public class InitService {
 
     @Inject
-    private TaskRepository repository;
-    @Inject
     private TaskService taskService;
 
     public ServiceResult init(List<Task> tasks) {
         // 过滤掉 contentType 是NONE的情况
-        List<Task> tasks1 = tasks.stream().filter(c -> !c.getContentType().equals("NONE")).collect(Collectors.toList());
-        repository.save((Iterable<Task>) () -> tasks1.iterator());
-        if (tasks.size() == repository.countAll()) {
-            // success
-            for (int i = 0;i < tasks.size();i++) {
-                System.out.println("InitController init task ->job : " + i + "  total : " + tasks.size());
-                taskService.saveTask(tasks.get(i));
-            }
+        try {
+            tasks.stream().filter(c -> !c.getContentType().equals("NONE")).collect(Collectors.toList()).forEach(taskService::saveTask);
             return ServiceResult.success("添加成功");
-        } else {
-            //fail
-            repository.delete(() -> tasks1.iterator());
+        } catch (AppException e) {
             return ServiceResult.failure("添加失败");
         }
+
     }
 
 }
