3a79aa9091fe35120f6a748f083127f4b51db11b
nobita <1057735161@qq.com>
2018-12-25 20:44:22 +0800

    取消更改

--- idc-app/src/main/java/com/iwellmass/idc/app/controller/JobInstanceController.java
+++ idc-app/src/main/java/com/iwellmass/idc/app/controller/JobInstanceController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.iwellmass.common.exception.AppException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,66 +29,66 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/job-instance")
 public class JobInstanceController {
-	
-	@Inject
-	private JobInstanceService jobInstanceService;
 
-	@Inject
-	private JobInstanceQueryService queryService;
+    @Inject
+    private JobInstanceService jobInstanceService;
 
-	@ApiOperation("通过条件检索实例（分页显示）")
-	@PostMapping("/query")
-	public ServiceResult<PageData<JobInstance>> queryJobInstance(
-			@RequestBody(required = false) JobInstanceQuery query, Pager pager) {
-		PageData<JobInstance> taskInstance = queryService.findJobInstance(query, pager);
-		return ServiceResult.success(taskInstance);
-	}
-	
-	@ApiOperation("获取所有责任人")
-	@GetMapping("/assignee")
-	public ServiceResult<List<Assignee>> assignee() {
-		return ServiceResult.success(queryService.getAllAssignee());
-	}
+    @Inject
+    private JobInstanceQueryService queryService;
 
-	@ApiOperation("获取子任务实例")
-	@GetMapping("/{id}/sub-job-instance")
-	public ServiceResult<List<JobInstance>> getWorkflowTask(@PathVariable("id") Integer id) {
-		List<JobInstance> result = queryService.getWorkflowSubInstance(id);
-		return ServiceResult.success(result);
-	}
+    @ApiOperation("通过条件检索实例（分页显示）")
+    @PostMapping("/query")
+    public ServiceResult<PageData<JobInstance>> queryJobInstance(
+            @RequestBody(required = false) JobInstanceQuery query, Pager pager) {
+        PageData<JobInstance> taskInstance = queryService.findJobInstance(query, pager);
+        return ServiceResult.success(taskInstance);
+    }
 
-	@ApiOperation("重跑任务")
-	@PostMapping("/{id}/redo")
-	public ServiceResult<String> restart(@PathVariable(name = "id") Integer id, @RequestBody (required = false) RedoRequest redoRequest) {
-		
-		if (redoRequest == null) {
-			redoRequest = new RedoRequest();
-		}
-		redoRequest.setInstanceId(id);
-		jobInstanceService.redo(redoRequest);
-		return ServiceResult.success("success");
-	}
-	
-	@ApiOperation("取消任务")
-	@PostMapping("/{id}/cancle")
-	public ServiceResult<String> cancle(@PathVariable(name = "id") Integer id, @RequestBody (required = false) CancleRequest redoRequest) {
-		redoRequest.setInstanceId(id);
-		jobInstanceService.cancle(redoRequest);
-		return ServiceResult.success("success");
-	}
-	
-	@ApiOperation("强制结束任务")
-	@PostMapping("/{id}/force-complete")
-	public ServiceResult<String> forceComplete(@PathVariable(name = "id") Integer id) {
-		jobInstanceService.forceComplete(id);
-		return ServiceResult.success("success");
-	}
-	
-	@ApiOperation("任务日志(分页)")
-	@PostMapping("/{id}/log")
-	public ServiceResult<PageData<ExecutionLog>> getLog(@PathVariable(name = "id") Integer id, Pager pager) {
-		PageData<ExecutionLog> data = jobInstanceService.getJobInstanceLog(id, pager);
-		return ServiceResult.success(data);
-	}
+    @ApiOperation("获取所有责任人")
+    @GetMapping("/assignee")
+    public ServiceResult<List<Assignee>> assignee() {
+        return ServiceResult.success(queryService.getAllAssignee());
+    }
+
+    @ApiOperation("获取子任务实例")
+    @GetMapping("/{id}/sub-job-instance")
+    public ServiceResult<List<JobInstance>> getWorkflowTask(@PathVariable("id") Integer id) {
+        List<JobInstance> result = queryService.getWorkflowSubInstance(id);
+        return ServiceResult.success(result);
+    }
+
+    @ApiOperation("重跑任务")
+    @PostMapping("/{id}/redo")
+    public ServiceResult<String> restart(@PathVariable(name = "id") Integer id, @RequestBody(required = false) RedoRequest redoRequest) {
+
+        if (redoRequest == null) {
+            redoRequest = new RedoRequest();
+        }
+        redoRequest.setInstanceId(id);
+        jobInstanceService.redo(redoRequest);
+        return ServiceResult.success("success");
+    }
+
+    @ApiOperation("取消任务")
+    @PostMapping("/{id}/cancle")
+    public ServiceResult<String> cancle(@PathVariable(name = "id") Integer id, @RequestBody(required = false) CancleRequest redoRequest) {
+        redoRequest.setInstanceId(id);
+        jobInstanceService.cancle(redoRequest);
+        return ServiceResult.success("success");
+    }
+
+    @ApiOperation("强制结束任务")
+    @PostMapping("/{id}/force-complete")
+    public ServiceResult<String> forceComplete(@PathVariable(name = "id") Integer id) {
+        jobInstanceService.forceComplete(id);
+        return ServiceResult.success("success");
+    }
+
+    @ApiOperation("任务日志(分页)")
+    @PostMapping("/{id}/log")
+    public ServiceResult<PageData<ExecutionLog>> getLog(@PathVariable(name = "id") Integer id, Pager pager) {
+        PageData<ExecutionLog> data = jobInstanceService.getJobInstanceLog(id, pager);
+        return ServiceResult.success(data);
+    }
 
 }
--- idc-core/src/main/java/com/iwellmass/idc/executor/IDCJob.java
+++ idc-core/src/main/java/com/iwellmass/idc/executor/IDCJob.java
@@ -1,9 +1,5 @@
 package com.iwellmass.idc.executor;
 
-import com.iwellmass.common.util.PageData;
-import com.iwellmass.common.util.Pager;
-import com.iwellmass.idc.model.JobLog;
-
 /**
  * 处理 {@link IDCJob#getContentType()} 类型的任务
  */
@@ -14,7 +10,4 @@ public interface IDCJob {
 	
 	// ~~ 业务逻辑 ~~
 	void execute(IDCJobExecutionContext context);
-
-	// ~~ 获取实例运行日志 ~~
-    PageData<JobLog> getRuntimeLog(Integer instanceId, Pager pager);
 }
