9a6f6d68caea9616ef4031ad2eaba6b4ff42a466
chenxiong <cx19970522>
2019-07-25 10:02:05 +0800

    update:1.default open scheduler  2.change json method for localdatetime

--- idc-app/src/main/java/com/iwellmass/idc/scheduler/model/Workflow.java
+++ idc-app/src/main/java/com/iwellmass/idc/scheduler/model/Workflow.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -41,6 +42,7 @@ public class Workflow {
      * 修改日期
      */
     @Column(name = "updatetime")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updatetime;
 
     /**
