39066a7683313047d98b8364e51844560d2640e3
chenxiong <cx19970522>
2019-08-02 14:51:03 +0800

    update:define task state

--- idc-app/src/main/java/com/iwellmass/idc/scheduler/model/TaskState.java
+++ idc-app/src/main/java/com/iwellmass/idc/scheduler/model/TaskState.java
@@ -7,15 +7,23 @@ import io.swagger.annotations.ApiModelProperty;
  */
 public enum TaskState {
 
-	@ApiModelProperty("等待调度")
+	@ApiModelProperty("等待执行")
 	WAITING,
+	@ApiModelProperty("冻结")
 	PAUSED,
+	@ApiModelProperty("准备执行")
 	ACQUIRED,
+	@ApiModelProperty("执行阻塞")
 	BLOCKED,
+	@ApiModelProperty("冻结")
 	SUSPENDED,
+	@ApiModelProperty("执行异常")
 	ERROR,
+	@ApiModelProperty("未执行")
 	NONE,
+	@ApiModelProperty("正在执行")
 	NORMAL,
+	@ApiModelProperty("执行完成")
 	COMPLETE;
 	public boolean isTerminated() {
 		return this==COMPLETE||this==ERROR;
